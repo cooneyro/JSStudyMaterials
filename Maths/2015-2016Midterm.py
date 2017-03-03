5b6f3846e0ffceab1e6f4a7dc4215b1ddb7e7dda
cooooneyro <roberttcooney@gmail.com>
2017-03-03 18:50:21 +0000

    Added Q2

--- Maths/2015-2016Midterm.py
+++ Maths/2015-2016Midterm.py
@@ -18,6 +18,7 @@ def gcd(a, b):
         a, b = b, a % b
     return a
 
+
 sys.stdout.write(BLUE)
 print("Q1: Suppose we roll a red die and a green die\n")
 
@@ -29,7 +30,7 @@ s = ""
 for i in outcomes:
     for j in outcomes:
         s += str("{{{i:0},{j:0}}} ".format(i=i, j=j))
-    print(s)
+    print("\t {}".format(s))
     s = ""
 
 sys.stdout.write(BLUE)
@@ -44,15 +45,15 @@ for i in outcomes:
             count += 1
 common_divisor = gcd(count, len(outcomes) ** 2)
 (reduced_num, reduced_den) = (count / common_divisor, len(outcomes) ** 2 / common_divisor)
-print("{0:g}/{1:g}".format(reduced_num, reduced_den))
+print("\t{0:g}/{1:g}".format(reduced_num, reduced_den))
 
 sys.stdout.write(BLUE)
 # (iii)
 print("\n(iii)Define what it means for two events E and F to be independent.")
 sys.stdout.write(RESET)
-print("Two events E and F are independent if the order in which they occur doesn't matter. "
+print("\tTwo events E and F are independent if the order in which they occur doesn't matter. "
       "Alternatively, if observing one doesn't affect the other.")
-print("P(E ∩ F) = P(E)P(F)")
+print("\tP(E ∩ F) = P(E)P(F)")
 
 sys.stdout.write(BLUE)
 # (iv)
@@ -60,7 +61,35 @@ print("\n(iv) Let event E be that the sum of the dice equals 2 or 3 and event F 
       " the sum equals 3. Are E and F independent? Explain with reference to the "
       "definition given above")
 sys.stdout.write(RESET)
-print("No, E and F are not independent. Observing E affects the outcome of F. Using the mathematical definition "
+print("\tNo, E and F are not independent. Observing E affects the outcome of F. Using the mathematical definition "
       "P(E ∩ F) = P(E)P(F):")
-print("P(E ∩ F) = 1/36, P(E)P(F) = 1/12 * 1/18 = 1/216")
-print("Thus, E and F are dependent as P(E ∩ F) != P(E)P(F)")
+print("\tP(E ∩ F) = 1/36, P(E)P(F) = 1/12 * 1/18 = 1/216")
+print("\tThus, E and F are dependent as P(E ∩ F) != P(E)P(F)")
+
+sys.stdout.write(BLUE)
+# Q2
+print("\nQ2:")
+
+# (i)
+print("\n(i) State Bayes Rule")
+
+sys.stdout.write(RESET)
+print("\tP(E|F) = (P(F|E)P(E))/P(F)")
+
+sys.stdout.write(BLUE)
+print("\n(ii) Suppose 1% of computers are infected with a virus.There is an imperfect test for detecting the virus."
+      "When applied to a computer with the virus the test gives a positive result 90% of the time.")
+print("When applied to a computer which does not have the virus, the test gives a negative result 99% of the time."
+      "Suppose that the test is positive for a computer.What is the probability that the computer has the virus?")
+
+sys.stdout.write(RESET)
+prop_infected = 0.01
+false_pos = 0.01
+p_f_given_e = 0.9
+p_e = 0.9
+p_f = (p_e * prop_infected) + (false_pos*(1-prop_infected))
+p_e_given_f = (p_f_given_e*p_e)/p_f
+print("\tE = actually has virus, F = test positive for virus")
+print("\tP(F|E) = {}, P(E)={}, P(F) = (0.9*0.01 + 0.01*(1-0.01)) = {}".format(p_f_given_e, p_e, p_f))
+print("\tP(E|F) = ({}*{})/{} = {}%".format(p_f_given_e, p_e, p_f, p_e_given_f))
+
